58296f177429a34b48b2d495cee1cac62228a257
Troy Gidney <troy.gidney@lethbridgecollege.ca>
2020-01-10 13:20:51 -0700

    Initial commit

---
+++ app/src/main/java/me/troyg/assignment1mobileappdevelopment/Main.java
@@ -0,0 +1,22 @@
+package me.troyg.assignment1mobileappdevelopment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.root);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+
+    }
+
+}
