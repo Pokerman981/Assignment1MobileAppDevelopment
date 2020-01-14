f7d7cdf6d2bcd14370bf12a867ea1d64a2744123
TroyG <troy.gidney@lethbridgecollege.ca>
2020-01-14 11:22:42 -0700

    aync

--- app/src/main/java/me/troyg/assignment1mobileappdevelopment/Main.java
+++ app/src/main/java/me/troyg/assignment1mobileappdevelopment/Main.java
@@ -2,6 +2,9 @@ package me.troyg.assignment1mobileappdevelopment;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +16,26 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.root);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitle("Rawr xD UwU");
         toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setNavigationIcon(R.mipmap.icon);
 
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView imageView = findViewById(R.id.imageview);
+                if (view.isActivated()) {
+                    view.setActivated(false);
+                    imageView.setScaleType(ImageView.ScaleType.CENTER);
+                    button.setText(R.string.buttonOff);
+                } else {
+                    view.setActivated(true);
+                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    button.setText(R.string.buttonOn);
+                }
+            }
+        });
 
     }
 
