ad00284f75c97d5e157dc1c28c64e0fee488f860
s0511527 <troy.gidney@gmail.com>
2020-01-21 11:45:41 -0700

    Added register listeners method, also added new sub dir for resource files.

--- app/src/main/java/me/troyg/assignment1mobileappdevelopment/Main.java
+++ app/src/main/java/me/troyg/assignment1mobileappdevelopment/Main.java
@@ -2,7 +2,6 @@ package me.troyg.assignment1mobileappdevelopment;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toolbar;
@@ -19,6 +18,12 @@ public class Main extends AppCompatActivity {
         toolbar.setTitle("Rawr xD UwU");
         toolbar.setTitleTextColor(Color.WHITE);
 
+        registerListeners();
+
+
+    }
+
+    private void registerListeners() {
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(view -> {
             ImageView imageView = findViewById(R.id.imageview);
@@ -32,7 +37,6 @@ public class Main extends AppCompatActivity {
                 button.setText(R.string.buttonOn);
             }
         });
-
     }
 
 }
