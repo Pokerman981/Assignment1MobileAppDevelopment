056d2cf800eea9927621ccad5372eaabca6a3576
s0511527 <troy.gidney@gmail.com>
2020-01-21 10:32:04 -0700

    sync

--- app/src/main/java/me/troyg/assignment1mobileappdevelopment/Main.java
+++ app/src/main/java/me/troyg/assignment1mobileappdevelopment/Main.java
@@ -18,22 +18,18 @@ public class Main extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Rawr xD UwU");
         toolbar.setTitleTextColor(Color.WHITE);
-        //toolbar.setNavigationIcon(R.mipmap.icon);
 
         final Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ImageView imageView = findViewById(R.id.imageview);
-                if (view.isActivated()) {
-                    view.setActivated(false);
-                    imageView.setScaleType(ImageView.ScaleType.CENTER);
-                    button.setText(R.string.buttonOff);
-                } else {
-                    view.setActivated(true);
-                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                    button.setText(R.string.buttonOn);
-                }
+        button.setOnClickListener(view -> {
+            ImageView imageView = findViewById(R.id.imageview);
+            if (view.isActivated()) {
+                view.setActivated(false);
+                imageView.setScaleType(ImageView.ScaleType.CENTER);
+                button.setText(R.string.buttonOff);
+            } else {
+                view.setActivated(true);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                button.setText(R.string.buttonOn);
             }
         });
 
